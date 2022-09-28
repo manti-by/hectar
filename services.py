272ea316cba2f7617b299bab2c45df20c762da9b
Alexander Chaika <manti.by@gmail.com>
2022-09-28 21:53:56 +0300

    Update server and bot

--- services.py
+++ services.py
@@ -1,5 +1,6 @@
 import os
 
+import aiohttp
 from telegram import Update, Bot
 from telegram.ext import ContextTypes
 
@@ -13,6 +14,16 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 async def message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f"Message {update.effective_user.first_name}")
+
+
+async def top_products(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    async with aiohttp.ClientSession() as session:
+        async with session.get("http://127.0.0.1:8000/api/products/?ordering=-cost") as response:
+            products = await response.json()
+    result = ""
+    for product in products["results"]:
+        result += f"{product['title']} - {product['cost']}\n"
+    await update.message.reply_text(result)
 
 
 async def send_message(text: str) -> None:
