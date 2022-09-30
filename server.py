d28a3ff68a9cfdb7016db63be4ca50a6cf9531e3
Alexander Chaika <manti.by@gmail.com>
2022-09-30 13:37:50 +0300

    Add API swagger

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
-from json import JSONDecodeError
-
+from aiohttp_apispec import docs, request_schema, setup_aiohttp_apispec
 from aiohttp import web
+from json import JSONDecodeError
 from marshmallow import ValidationError
 
 from schemas import MessageSchema
@@ -9,8 +9,15 @@ from services import send_message
 routes = web.RouteTableDef()
 
 
+@docs(
+    tags=["telegram"],
+    summary="Send message API",
+    description="This end-point sends message to telegram bot user/users",
+)
+@request_schema(MessageSchema())
 @routes.post("/")
 async def index_get(request: web.Request) -> web.Response:
+    ...
     try:
         payload = await request.json()
     except JSONDecodeError:
@@ -30,5 +37,9 @@ async def index_get(request: web.Request) -> web.Response:
 
 if __name__ == "__main__":
     app = web.Application()
+    setup_aiohttp_apispec(
+        app=app, title="Hectar Bot documentation", version="v1.0",
+        url="/api/docs/swagger.json", swagger_path="/api/docs",
+    )
     app.add_routes(routes)
     web.run_app(app, port=5000)
