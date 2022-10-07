71e2f5d5e29d564c0b386976e7892d96f276126f
Alexander Chaika <manti.by@gmail.com>
2022-10-07 15:08:36 +0300

    Add processes and threads

---
+++ processes.py
@@ -0,0 +1,19 @@
+import random
+import os
+
+from multiprocessing import Process
+from time import sleep
+
+
+def random_time():
+    for _ in range(10):
+        print(f"From process: {os.getpid()}")
+        sleep(random.randint(1, 3))
+
+
+if __name__ == "__main__":
+    process_1 = Process(target=random_time)
+    process_1.start()
+
+    process_2 = Process(target=random_time)
+    process_2.start()
---
+++ threads.py
@@ -0,0 +1,18 @@
+import random
+
+from threading import Thread, get_ident
+from time import sleep
+
+
+def threaded_function():
+    for _ in range(10):
+        print(f"From thread: {get_ident()}")
+        sleep(random.randint(1, 3))
+
+
+if __name__ == "__main__":
+    thread_1 = Thread(target=threaded_function)
+    thread_1.start()
+
+    thread_2 = Thread(target=threaded_function)
+    thread_2.start()
